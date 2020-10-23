7f6c4634abb9840c2e80dadda807f5236f7941fd
Mohamed Daher <madaher@alumni.ie.edu>
2020-10-23 21:35:13 +0300

    putting static path in server.js

--- server.js
+++ server.js
@@ -27,6 +27,13 @@ mongoose
   })
   .then(() => console.log('DB connection successful!'));
 
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
