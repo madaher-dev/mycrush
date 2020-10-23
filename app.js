dba7f5444143bdd7e59b79d888ca0dec8e7ec95f
Mohamed Daher <madaher@alumni.ie.edu>
2020-10-23 21:28:17 +0300

    another cleanup

--- app.js
+++ app.js
@@ -69,8 +69,8 @@ app.use('/api/v1/users', userRouter);
 
 // 3-Serving static assets in production
 if (process.env.NODE_ENV === 'production') {
-  //Set static folder
-  app.use(express.static('client/buid'));
+  // Set static folder
+  app.use(express.static('client/build'));
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
